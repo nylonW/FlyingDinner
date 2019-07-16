5ca0ac6d5810acf76f06f60fca1f65fc68123bdc
Marcin Ślusarek <mrvrota@gmail.com>
2019-07-16 21:37:12 +0200

    Add textures and gameplay improvements

--- catchThis/GameScene/GameScene.swift
+++ catchThis/GameScene/GameScene.swift
@@ -9,12 +9,19 @@
 import SpriteKit
 import GameplayKit
 
+enum GameState {
+    case title, ready, playing, gameOver
+}
+
 class GameScene: SKScene {
     
     var touching = false
     var touchPoint = CGPoint()
     var catchableItem: CatchableItem?
     var items: [CatchableItem] = []
+    var playButton: MSButtonNode!
+    
+    var state: GameState = .title
     
     var scoreLabel: SKLabelNode!
     var score = 0 {
@@ -24,29 +31,38 @@ class GameScene: SKScene {
     }
     
     override func didMove(to view: SKView) {
-        catchableItem = childNode(withName: "catchableItem") as? CatchableItem
-        catchableItem?.setupItem()
-        // Get label node from scene and store it for use later
         
-//        catchableItem?.run(SKAction(named: "In")!, completion: { () -> Void in
-//            self.catchableItem?.physicsBody?.isDynamic = true
-//        })
+        playButton = childNode(withName: "playButton") as? MSButtonNode
+        let background = childNode(withName: "background") as! SKSpriteNode
+        background.texture?.filteringMode = SKTextureFilteringMode.nearest
         
-        catchableItem?.physicsBody?.velocity = CGVector(dx: -200, dy: 250)
-        
-        self.items.append(catchableItem!)
-        
-        scoreLabel = childNode(withName: "scoreLabel") as? SKLabelNode
-        
-        
-        let wait = SKAction.wait(forDuration: 1)
-        let update = SKAction.run( {
+        if state == .ready {
+            playButton.isHidden = true
+            let wait = SKAction.wait(forDuration: 1)
+            let update = SKAction.run( {
                 self.throwNewItem()
             })
+            
+            let seq = SKAction.sequence([wait,update])
+            let repeatAction = SKAction.repeatForever(seq)
+            self.run(repeatAction)
+        }
         
-        let seq = SKAction.sequence([wait,update])
-        let repeatAction = SKAction.repeatForever(seq)
-        run(repeatAction)
+        playButton.selectedHandler = {
+            self.state = .ready
+            self.playButton.isHidden = true
+            
+            let wait = SKAction.wait(forDuration: 1)
+            let update = SKAction.run( {
+                self.throwNewItem()
+            })
+            
+            let seq = SKAction.sequence([wait,update])
+            let repeatAction = SKAction.repeatForever(seq)
+            self.run(repeatAction)
+        }
+        
+        scoreLabel = childNode(withName: "scoreLabel") as? SKLabelNode
         
     }
     
@@ -59,10 +75,10 @@ class GameScene: SKScene {
         
         let random = arc4random_uniform(100)
         if random <= 49 {
-            item.position = CGPoint(x: 367, y: 194)
+            item.position = CGPoint(x: 367, y: 220)
             item.physicsBody?.velocity = CGVector(dx: -600 - 1.5 * CGFloat(score), dy: CGFloat(400 + 2 * score))
         } else {
-            item.position = CGPoint(x: -40, y: 194)
+            item.position = CGPoint(x: -40, y: 220)
             item.physicsBody?.velocity = CGVector(dx: 600 + 1.5 * CGFloat(score), dy: CGFloat(400 + 2 * score)                                                                   )
         }
         
@@ -71,12 +87,20 @@ class GameScene: SKScene {
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
+        if state == .gameOver || state == .title {
+            playButton.isHidden = false
+            return
+        }
+        
+        if state == .ready {
+            state = .playing
+            playButton.isHidden = true
+        }
+        
         let touch = touches.first!
         let location = touch.location(in: self)
         for catchableItem in items {
             if catchableItem.frame.contains(location) {
-                catchableItem.removeAllActions()
-                catchableItem.physicsBody?.isDynamic = true
                 touchPoint = location
                 catchableItem.touching = true
             }
@@ -86,7 +110,9 @@ class GameScene: SKScene {
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         let touch = touches.first!
         let location = touch.location(in: self)
+        
         touchPoint = location
+        
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -98,6 +124,15 @@ class GameScene: SKScene {
     override func update(_ currentTime: TimeInterval) {
         
         items.removeAll(where: { $0.removed })
+
+        if state == .gameOver {
+            self.playButton.isHidden = false
+            removeItems()
+            return
+        } else if state != .playing {
+            removeItems()
+            return
+        }
 
         for item in items {
             if item.touching {
@@ -111,10 +146,36 @@ class GameScene: SKScene {
                 score += 1
                 item.removeFromParent()
                 item.removed = true
+            } else if item.position.y < 0 && score != 0 {
+                gameOver()
+                item.removeFromParent()
+                item.removed = true
             } else if item.position.y < 0 {
                 item.removeFromParent()
                 item.removed = true
             }
+        }
+    }
+    
+    func removeItems() {
+        for item in items {
+            if item.position.y < 0 {
+                item.removeFromParent()
+                item.removed = true
+            }
+        }
+    }
+    
+    func gameOver() {
+        state = .gameOver
+        
+        playButton.selectedHandler = {
+            let skView = self.view as SKView?
+            guard let scene = SKScene(fileNamed: "GameScene") as! GameScene? else { return }
+            
+            scene.scaleMode = .aspectFill
+            scene.state = .ready
+            skView?.presentScene(scene)
         }
     }
 }
--- catchThis/Items/CatchableItem.swift
+++ catchThis/Items/CatchableItem.swift
@@ -11,8 +11,13 @@ import SpriteKit
 class CatchableItem: SKSpriteNode {
     
     var catched = false
-    var removed = false
+    var removed = false {
+        didSet {
+            trailNode.removeFromParent()
+        }
+    }
     var touching = false
+    var trailNode: SKNode!
     
     override init(texture: SKTexture?, color: UIColor, size: CGSize) {
         super.init(texture: texture, color: color, size: size)
@@ -24,8 +29,36 @@ class CatchableItem: SKSpriteNode {
     
     func setupItem() {
         self.physicsBody?.isDynamic = true
-        let texture = SKTexture(imageNamed: "item")
+        
+        var textures = [String]()
+        textures.append("Apple")
+        textures.append("Brownie")
+        textures.append("Eggs")
+        textures.append("Fish")
+        //textures.append("Honey")
+        textures.append("Shrimp")
+        textures.append("Strawberry")
+        //textures.append("Bomb")
+        //textures.append("Cat")
+        //textures.append("r2d2")
+        
+        let random = arc4random_uniform(UInt32(textures.count))
+        let texture = SKTexture(imageNamed: textures[Int(random)])
+        
+        trailNode = SKNode()
+        trailNode.physicsBody?.mass = 0
+        trailNode.zPosition = 0
+        self.parent?.addChild(trailNode)
+        let trail = SKEmitterNode(fileNamed: "spark.sks")!
+        trail.targetNode = trailNode
+        self.addChild(trail)
+
         self.size = CGSize(width: self.size.width * 3, height: self.size.height * 3)
-        self.physicsBody = SKPhysicsBody(texture: SKTexture(imageNamed: "item"), size: size)
+        
+        texture.filteringMode = SKTextureFilteringMode.nearest
+        self.texture = texture
+        self.physicsBody?.angularVelocity = 10
+        self.physicsBody?.allowsRotation = true
+        self.physicsBody = SKPhysicsBody(texture: texture, size: size)
     }
 }
